f2994f4d6dbbd1a1c47713ea64ca55e47b854043
Ahmed EL-Gammal <ahmedmabdalla91@gmail.com>
2024-01-24 14:22:56 +0200

    mails

--- app/Mail/InviteMeeting.php
+++ app/Mail/InviteMeeting.php
@@ -39,7 +39,7 @@ class InviteMeeting extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.invite',
         );
     }
 
