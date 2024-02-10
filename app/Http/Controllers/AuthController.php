34db9122d4dab900308e8d805a3671524c430e75
mohamed.ashry <mohamed.reda@safavisa.com>
2024-02-10 22:27:39 +0200

    add user to invitee

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Invitee;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
@@ -45,6 +46,11 @@ class AuthController extends Controller
                 'email' => $googleUser->email,
                 'google_token' => $googleUser->token,
                 'google_refresh_token' => $googleUser->refreshToken,
+            ]);
+
+            Invitee::create([
+                'email' => $user->email,
+                'name' => $user->name
             ]);
 
             Auth::login($user);
