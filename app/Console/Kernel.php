f21703867e275c3180a289284d5985cc803143d0
mohamed.ashry <mohamed.reda@safavisa.com>
2024-02-13 02:16:37 +0200

    test reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('php artisan app:reminder-meeting')->everyMinute();
+        $schedule->command('app:reminder-meeting')->everyMinute();
     }
 
     /**
